b09c8018c03f52b72d8230581d3f2bc54b936e4d
Moonlight1z <alexxcactucc@gmail.com>
2024-12-18 04:19:03 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
